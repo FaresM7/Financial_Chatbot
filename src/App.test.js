410b9ddc0fc88b11255c1283cc05159205f8a051
F <fares@gmail.com>
2024-07-03 13:59:28 +0200

    Modified on package.json and app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import { render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders chat assistant header', async () => {
   await act(async () => {
     render(<App />);
   });
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/your chat assistant :\)/i);
+  expect(headerElement).toBeInTheDocument();
 });
